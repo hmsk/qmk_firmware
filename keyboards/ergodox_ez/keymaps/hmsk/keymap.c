136374cc162abba5b856803f1b474558c4d4220d
hmsk <k.hamasaki@gmail.com>
2018-06-17 23:41:38 -0700

    Have same cursor key between layers 0 and 1

--- keyboards/ergodox_ez/keymaps/hmsk/keymap.c
+++ keyboards/ergodox_ez/keymaps/hmsk/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Ctrl |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           | +Spc |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1| Left | Right| LAlt | LGui |                                       |   [  |   ]  |  Up  | Down | ~L1  |
+ *   |Grv/L1| Left |  Up  | LAlt | LGui |                                       |   [  |   ]  | Down | Right| ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_LEFT,      KC_RIGHT,  KC_LALT,KC_LGUI,
+        LT(SYMB,KC_GRV),KC_LEFT,      KC_UP,  KC_LALT,KC_LGUI,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
              LGUI(KC_SPC),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_LBRC, KC_RBRC,KC_UP,KC_DOWN,          KC_FN1,
+                                  KC_LBRC, KC_RBRC,KC_DOWN,KC_RIGHT,       KC_FN1,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
              KC_QUOT,KC_TAB, KC_ENT
